f2d09699b2c7bbd254aaaa237d6a65a98fcad80c
odghe16 <121343407+odghe16@users.noreply.github.com>
2024-02-10 17:05:43 +0100

    Completed code for 1st challenge
    The very first challenge on JavaScript fundamentals. A basic program to compare Body-Mass Indexes between two persons, using the fundamental concepts of variables, math operators and more.

---
+++ challenge_one.js
@@ -0,0 +1,14 @@
+// Body Mass Index Comparison
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+
+console.log(BMIMark);
+console.log(BMIJohn);
+
+const markHigherBMI = BMIMark > BMIJohn;
+console.log(markHigherBMI);
