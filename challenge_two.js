111edd9363382c2ae6bdd18e9c66c6933d4926a6
odghe16 <121343407+odghe16@users.noreply.github.com>
2024-02-10 18:07:40 +0100

    Completed the 2nd JavaScript challenge..
    Similar to the previous challenge, I wrote code to compare the BMI Indexes of two different people, but with the use of if-else statements for the decision-making process.

---
+++ challenge_two.js
@@ -0,0 +1,19 @@
+// Comparing Mark and John's Body-Mass Indexes using a "control structure"
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+
+/* Write your code below. Good luck! 🙂 */
+
+console.log("Mark's BMI is higher than John's!");
+if (BMIMark > BMIJohn) {
+    console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})`);
+} else {
+    console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})`);
+}
