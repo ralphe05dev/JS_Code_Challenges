0d016750a07a69f8e45ba3412b469ad2d673f2e6
odghe16 <121343407+odghe16@users.noreply.github.com>
2024-02-11 18:01:36 +0100

    Completed 3rd JS Challenge
    Made use of if-else statements and boolean logic to compare the scores between two gymnastics teams and made different outputs for different scenarios.

---
+++ udemy_course_challenges/challenge_three.js
@@ -0,0 +1,30 @@
+/* There are two gymnastics teams: Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins a trophy!
+
+Your tasks:
+
+1. Calculate the average score for each team, using the test data included below. The average score for Dolphins should be assigned to the scoreDolphins variable, and the average score of Koalas should be assigned to the scoreKoalas variable.
+
+2. Compare the team's average scores to determine the winner of the competition, and print to the console:
+
+"Dolphins win the trophy" if Dolphins win, or
+
+"Koalas win the trophy" if Koalas win, or
+
+"Both win the trophy" if their average scores are equal.
+
+
+
+TEST DATA: Dolphins scored 96, 108, and 89. Koalas scored 88, 91, and 110. */
+
+// Using last pair of test data
+const scoreDolphins = 89;
+const scoreKoalas = 110;
+if (scoreDolphins > scoreKoalas) {
+    console.log("Dolphins win the trophy");
+} else if (scoreKoalas > scoreDolphins) {
+    console.log("Koalas win the trophy");
+} else if (scoreKoalas === scoreDolphins) {
+    console.log("Both win the trophy");
+}
+
+// OUTPUT: Koalas win the trophy
