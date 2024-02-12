3dac04ed593ea7c322bcd7135436eecf8fdea3f4
ralphe05dev <raphaelodeareduo71@gmail.com>
2024-02-12 21:22:34 +0100

    Completed code for 6th JS Challenge
    Refactored code for previously built tip calculator. Made use of function declaration and if-else statement for decision making, with a return keyword to get the tip value and end the function. Also stored various values of bills with Array data structure.

---
+++ udemy_course_challenges/challenge_6.js
@@ -0,0 +1,31 @@
+/* Steven wants you to improve his tip calculator, using the same rules as before — tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+
+Your tasks:
+
+Write a function calcTip that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from the first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
+
+And now let's use arrays! So, create an array called bills containing the test data below.
+
+Create an array called tips containing the tip value for each bill, calculated from the function you created before.
+
+BONUS: Create an array totals containing the total values, so the bill + tip.
+
+TEST DATA: 125, 555, and 44. */
+
+function calcTip(bill) {
+  let tip;
+  if (bill >= 50 && bill <= 300) {
+      tip = 0.15 * bill;
+  } else {
+      tip = 0.20 * bill;
+  }
+  return tip;
+}
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2]),];
+const totals = [ bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2] ];
+
+console.log(bills);
+console.log(tips);
+console.log(totals);
